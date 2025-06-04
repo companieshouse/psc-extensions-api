dafc143b74863343c308309c9e30e0293d1d67e2
rosey-mccloskey-ch <rmccloskey@companieshouse.gov.uk>
2025-06-04 14:40:39 +0100

    SIV-315: created skeleton for project

---
+++ src/main/java/uk.gov.companieshouse.psc.extensions/configuration/MongoConfig.java
@@ -0,0 +1,29 @@
+package uk.gov.companieshouse.psc.extensions.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Configuration
+@EnableMongoRepositories( "uk.gov.companieshouse.psc.extensions.repositories" ) //TODO: Create this folder
+@EnableMongoAuditing( dateTimeProviderRef = "mongodbDatetimeProvider" )
+public class MongoConfig {
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener( final LocalValidatorFactoryBean factory ) {
+        return new ValidatingMongoEventListener( factory );
+    }
+
+    @Bean( name = "mongodbDatetimeProvider" )
+    public DateTimeProvider dateTimeProvider() {
+        return () -> Optional.of( LocalDateTime.now() );
+    }
+
+}
