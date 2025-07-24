d03e53dee9b8667469af6b9b7cadc7183dcbe5ee
ehenry-ch <ehenry@companieshouse.gov.uk>
2025-07-24 17:27:03 +0100

    pr changes

--- src/test/java/uk/gov/companieshouse/psc/extensions/api/HealthCheckControllerTest.java
+++ src/test/java/uk/gov/companieshouse/psc/extensions/api/HealthCheckControllerTest.java
@@ -21,7 +21,7 @@ class HealthCheckControllerTest {
 
   public HealthCheckControllerTest(
       final @Value(
-          "${management.endpoints.web.path-mapping.health:/actuator/health}") String healthcheckUrl
+          "${management.endpoints.web.path-mapping.health:/psc-extensions-api/healthcheck}") String healthcheckUrl
   ) {
     this.healthcheckUrl = healthcheckUrl;
   }
