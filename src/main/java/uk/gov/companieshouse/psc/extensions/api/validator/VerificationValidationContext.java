3ebc736de380513aed640e89330058887bf42e2f
marychacia-ch <mchacia@companieshouse.gov.uk>
2025-09-22 15:11:36 +0100

    Validation for the PSC extension

--- src/main/java/uk/gov/companieshouse/psc/extensions/api/validator/VerificationValidationContext.java
+++ src/main/java/uk/gov/companieshouse/psc/extensions/api/validator/VerificationValidationContext.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.StringJoiner;
 
-/**
+/*
  * Record for passing context into {@link VerificationValidationService}.
  */
 public record VerificationValidationContext(@NonNull PscVerificationData dto,
