24e12746754580ba8bbd07728773e09654aec14e
ehenry27 <ehenry@companieshouse.gov.uk>
2025-11-06 15:56:50 +0000

    Refactor

--- src/main/java/uk/gov/companieshouse/psc/extensions/api/config/WebSecurityConfig.java
+++ src/main/java/uk/gov/companieshouse/psc/extensions/api/config/WebSecurityConfig.java
@@ -13,30 +13,40 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.csrf.CsrfFilter;
 import uk.gov.companieshouse.api.filter.CustomCorsFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer  {
 
   private static final Supplier<List<String>> externalMethods = () -> List.of( GET.name() );
 
 
   @Bean
   public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
-    return http.cors(AbstractHttpConfigurer::disable)
+    http.cors(AbstractHttpConfigurer::disable)
         .csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .addFilterBefore(new CustomCorsFilter(externalMethods.get() ), CsrfFilter.class )
         .authorizeHttpRequests(request -> request
-            .requestMatchers(POST, "/transactions/*/persons-with-significant-control-extensions").permitAll()
-            .requestMatchers(GET, "/persons-with-significant-control-extensions/healthcheck").permitAll()
-            .requestMatchers(GET, "/persons-with-significant-control-extensions/*/extensionCount").permitAll()
-            .requestMatchers(GET, "/persons-with-significant-control-extensions/*/*/isPscExtensionRequestValid").permitAll()
-            .requestMatchers(GET, "/private/transactions/*/persons-with-significant-control-extensions/*/filings").permitAll()
-            .requestMatchers(GET, "/transactions/*/persons-with-significant-control-extensions/*/validation_status").permitAll()
-            .anyRequest().denyAll()
-        ).build();
+            .requestMatchers(POST, "/transactions/*/persons-with-significant-control-extensions")
+            .permitAll()
+            .requestMatchers(GET, "/persons-with-significant-control-extensions/healthcheck")
+            .permitAll()
+            .requestMatchers(GET, "/persons-with-significant-control-extensions/*/extensionCount")
+            .permitAll()
+            .requestMatchers(GET, "/persons-with-significant-control-extensions/*/*/isPscExtensionRequestValid")
+            .permitAll()
+            .requestMatchers(GET, "/private/transactions/*/persons-with-significant-control-extensions/*/filings")
+            .permitAll()
+            .requestMatchers(GET, "/transactions/*/persons-with-significant-control-extensions/*/validation_status")
+            .permitAll()
+            .anyRequest()
+            .denyAll());
+
+    return http.build();
+
   }
 
 }
