ab20ea8d2d8d9ae3373aa7f48d990e697ddf0eb7
pspeasegood-ch <ppeasegood@companieshouse.gov.uk>
2025-12-15 15:26:59 +0000

    added tests

--- src/test/java/uk/gov/companieshouse/psc/extensions/api/service/impl/PscLookupServiceImplTest.java
+++ src/test/java/uk/gov/companieshouse/psc/extensions/api/service/impl/PscLookupServiceImplTest.java
@@ -48,4 +48,134 @@ class PscLookupServiceImplTest {
     void serviceFields_ShouldNotBeNull() {
         assertNotNull(pscLookupService);
     }
+
+
+    @Test
+    void getPscIndividualFullRecord_success_returnsData() throws Exception {
+        // Arrange
+        String companyNumber = "12345678";
+        String pscId = "psc-123";
+        uk.gov.companieshouse.psc.extensions.api.enumerations.PscType pscType =
+                uk.gov.companieshouse.psc.extensions.api.enumerations.PscType.INDIVIDUAL;
+
+        uk.gov.companieshouse.api.InternalApiClient internalApiClient =
+                org.mockito.Mockito.mock(uk.gov.companieshouse.api.InternalApiClient.class, org.mockito.Mockito.RETURNS_DEEP_STUBS);
+
+        uk.gov.companieshouse.api.psc.IndividualFullRecord fullRecord =
+                new uk.gov.companieshouse.api.psc.IndividualFullRecord();
+
+        // Deep-stub the full chain to return the fullRecord from getData()
+        org.mockito.Mockito.when(
+                internalApiClient.privatePscFullRecordResourceHandler()
+                        .getPscFullRecord("/company/" + companyNumber + "/persons-with-significant-control/" + pscType.getValue() + "/" + pscId + "/full_record")
+                        .execute()
+                        .getData()
+        ).thenReturn(fullRecord);
+
+        org.mockito.Mockito.when(internalApiClientService.getInternalApiClient()).thenReturn(internalApiClient);
+
+        // Act
+        uk.gov.companieshouse.api.psc.IndividualFullRecord result =
+                pscLookupService.getPscIndividualFullRecord(companyNumber, pscId, pscType);
+
+        // Assert
+        org.junit.jupiter.api.Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void getPscIndividualFullRecord_apiError404_throwsFilingResourceNotFoundException() throws Exception {
+        // Arrange
+        String companyNumber = "12345678";
+        String pscId = "psc-404";
+        uk.gov.companieshouse.psc.extensions.api.enumerations.PscType pscType =
+                uk.gov.companieshouse.psc.extensions.api.enumerations.PscType.INDIVIDUAL;
+
+        uk.gov.companieshouse.api.InternalApiClient internalApiClient =
+                org.mockito.Mockito.mock(uk.gov.companieshouse.api.InternalApiClient.class, org.mockito.Mockito.RETURNS_DEEP_STUBS);
+
+        // Throw ApiErrorResponseException (404) from execute()
+        org.mockito.Mockito.when(
+                internalApiClient.privatePscFullRecordResourceHandler()
+                        .getPscFullRecord("/company/" + companyNumber + "/persons-with-significant-control/" + pscType.getValue() + "/" + pscId + "/full_record")
+                        .execute()
+        ).thenThrow(
+                new uk.gov.companieshouse.api.error.ApiErrorResponseException(
+                        new com.google.api.client.http.HttpResponseException.Builder(
+                                org.springframework.http.HttpStatus.NOT_FOUND.value(),
+                                "Not Found",
+                                new com.google.api.client.http.HttpHeaders()
+                        )
+                )
+        );
+
+        org.mockito.Mockito.when(internalApiClientService.getInternalApiClient()).thenReturn(internalApiClient);
+
+        // Act + Assert
+        org.junit.jupiter.api.Assertions.assertThrows(
+                uk.gov.companieshouse.psc.extensions.api.exception.FilingResourceNotFoundException.class,
+                () -> pscLookupService.getPscIndividualFullRecord(companyNumber, pscId, pscType)
+        );
+    }
+
+    @Test
+    void getPscIndividualFullRecord_apiError500_throwsPscLookupServiceException() throws Exception {
+        // Arrange
+        String companyNumber = "12345678";
+        String pscId = "psc-500";
+        uk.gov.companieshouse.psc.extensions.api.enumerations.PscType pscType =
+                uk.gov.companieshouse.psc.extensions.api.enumerations.PscType.INDIVIDUAL;
+
+        uk.gov.companieshouse.api.InternalApiClient internalApiClient =
+                org.mockito.Mockito.mock(uk.gov.companieshouse.api.InternalApiClient.class, org.mockito.Mockito.RETURNS_DEEP_STUBS);
+
+        // Throw ApiErrorResponseException (500) from execute()
+        org.mockito.Mockito.when(
+                internalApiClient.privatePscFullRecordResourceHandler()
+                        .getPscFullRecord("/company/" + companyNumber + "/persons-with-significant-control/" + pscType.getValue() + "/" + pscId + "/full_record")
+                        .execute()
+        ).thenThrow(
+                new uk.gov.companieshouse.api.error.ApiErrorResponseException(
+                        new com.google.api.client.http.HttpResponseException.Builder(
+                                500, "Internal Error", new com.google.api.client.http.HttpHeaders()
+                        )
+                )
+        );
+
+        org.mockito.Mockito.when(internalApiClientService.getInternalApiClient()).thenReturn(internalApiClient);
+
+        // Act + Assert
+        org.junit.jupiter.api.Assertions.assertThrows(
+                uk.gov.companieshouse.psc.extensions.api.exception.PscLookupServiceException.class,
+                () -> pscLookupService.getPscIndividualFullRecord(companyNumber, pscId, pscType)
+        );
+    }
+
+    @Test
+    void getPscIndividualFullRecord_uriValidation_throwsPscLookupServiceException() throws Exception {
+        // Arrange
+        String companyNumber = "12345678";
+        String pscId = "psc-uri";
+        uk.gov.companieshouse.psc.extensions.api.enumerations.PscType pscType =
+                uk.gov.companieshouse.psc.extensions.api.enumerations.PscType.INDIVIDUAL;
+
+        uk.gov.companieshouse.api.InternalApiClient internalApiClient =
+                org.mockito.Mockito.mock(uk.gov.companieshouse.api.InternalApiClient.class, org.mockito.Mockito.RETURNS_DEEP_STUBS);
+
+        // Throw URIValidationException from execute()
+        org.mockito.Mockito.when(
+                internalApiClient.privatePscFullRecordResourceHandler()
+                        .getPscFullRecord("/company/" + companyNumber + "/persons-with-significant-control/" + pscType.getValue() + "/" + pscId + "/full_record")
+                        .execute()
+        ).thenThrow(new uk.gov.companieshouse.api.handler.exception.URIValidationException("bad uri"));
+
+        org.mockito.Mockito.when(internalApiClientService.getInternalApiClient()).thenReturn(internalApiClient);
+
+        // Act + Assert
+        org.junit.jupiter.api.Assertions.assertThrows(
+                uk.gov.companieshouse.psc.extensions.api.exception.PscLookupServiceException.class,
+                () -> pscLookupService.getPscIndividualFullRecord(companyNumber, pscId, pscType)
+        );
+
+    }
+
 }
